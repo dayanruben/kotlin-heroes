fd517be466d97159ce1467cedd9cb8d355af6be0
Dayan Ruben <mail@dayanruben.com>
2025-04-07 13:21:58 -0300

    Refactor withFile to pass InputStream to block

--- src/utils/File.kt
+++ src/utils/File.kt
@@ -3,19 +3,21 @@ package utils
 import java.io.Closeable
 import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 import java.io.PrintStream
 
 fun <T : Closeable, R> T.useWith(block: T.() -> R): R = use { with(it, block) }
 
-fun withFile(input: String = "test.in", output: String = "test.out", block: () -> Unit) {
+fun withFile(input: String = "test.in", output: String = "test.out", block: (InputStream) -> Unit) {
     val originalIn = System.`in`
     val originalOut = System.out
 
     try {
-        System.setIn(FileInputStream(input))
+        val inputStream = FileInputStream(input)
+        System.setIn(inputStream)
         System.setOut(PrintStream(File(output)))
 
-        block()
+        block(inputStream)
     } finally {
         System.setIn(originalIn)
         System.setOut(originalOut)
