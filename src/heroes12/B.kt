62ba8c3f43aa365ef64a42c2d4f5b8abd6e4979a
Dayan Ruben <mail@dayanruben.com>
2025-04-07 11:54:22 -0300

    Solved problem B

--- src/heroes12/B.kt
+++ src/heroes12/B.kt
@@ -1,11 +1,23 @@
 package heroes12
 
-import utils.withFile
-
-fun main() = withFile {
+fun main() {
     val t = readln().toInt()
     repeat(t) {
         val n = readln().toInt()
-        println(n - 1)
+        val a = readln().split(" ").map { it.toInt() }.sorted()
+        var ok = true
+        for (i in 0 until n) {
+            val l = 2 * i
+            val r = 2 * i + 1
+            if (r < a.size - 1 && a[r] - a[l] > a[r + 1] - a[r]) {
+                ok = false
+                break
+            }
+            if (l > 0 && a[r] - a[l] > a[l] - a[l - 1]) {
+                ok = false
+                break
+            }
+        }
+        println(if (ok) "YES" else "NO")
     }
 }
