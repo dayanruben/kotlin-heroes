58cdb798ef28ab6ff42d9cda8adb7961f13f81fd
Dayan Ruben <mail@dayanruben.com>
2025-04-07 11:41:07 -0300

    Solved problem A

--- src/heroes12/A.kt
+++ src/heroes12/A.kt
@@ -1,11 +1,36 @@
 package heroes12
 
-import utils.withFile
-
-fun main() = withFile {
+fun main() {
     val t = readln().toInt()
+    val digits = "0123456789"
+    val uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lowers = "abcdefghijklmnopqrstuvwxyz"
+
     repeat(t) {
-        val n = readln().toInt()
-        println(n - 1)
+        val (a, b, c) = readln().split(" ").map { it.toInt() }
+
+        val password = StringBuilder()
+        for (i in 0 until c) {
+            password.append(lowers[i % lowers.length])
+        }
+        for (i in 0 until b) {
+            password.append(uppers[i % uppers.length])
+        }
+        for (i in 0 until a) {
+            password.append(digits[i % digits.length])
+        }
+
+        for (i in 1 until password.length) {
+            if (password[i] == password[i - 1]) {
+                for (ch in (digits + lowers + uppers)) {
+                    if (ch != password[i - 1] && (i == password.length - 1 || ch != password[i + 1])) {
+                        password.setCharAt(i, ch)
+                        break
+                    }
+                }
+            }
+        }
+
+        println(password)
     }
 }
